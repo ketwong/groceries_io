bfe26ea107cbf83f8112f7612c6a65e82b5090f5
Ket Wong <kketbb@gmail.com>
2023-12-24 08:07:28 -0800

    Optimisation

---
+++ static/js/script.js
@@ -0,0 +1,35 @@
+document.getElementById('upload-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('loading').style.display = 'block';
+    var formData = new FormData(this);
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData
+    }).then(response => response.json())
+      .then(data => {
+          document.getElementById('loading').style.display = 'none';
+          document.getElementById('processing').style.display = 'block';
+
+          setTimeout(() => {
+              document.getElementById('processing').style.display = 'none';
+              document.getElementById('result').textContent = 'Result: ' + data.content;
+          }, 1000);
+      }).catch(error => {
+          document.getElementById('loading').style.display = 'none';
+          document.getElementById('processing').style.display = 'none';
+          console.error('Error:', error);
+          document.getElementById('result').textContent = 'Error: ' + error;
+      });
+});
+
+document.getElementById('image-input').addEventListener('change', function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var img = document.getElementById('image-preview');
+        img.src = e.target.result;
+        img.style.display = 'block';
+    };
+    reader.readAsDataURL(file);
+});
